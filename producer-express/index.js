5562f9e3c94198d9b188a7be4a05caef1bece9b8
mahenv <mahendrauppari044@gmail.com>
2025-09-06 12:53:39 -0500

    Allowed to access from anywhere

--- producer-express/index.js
+++ producer-express/index.js
@@ -122,4 +122,6 @@ app.post("/updatePlan", async (req, res) => {
 // });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => 
+  console.log(`Server running on http://0.0.0.0:${PORT}`)
+);
